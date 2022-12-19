5fcb2747a9f69113777564ee73c5239f0ce5fb1f
Yusron H <yusronzain@gmail.com>
2022-12-19 22:46:20 +0700

    add impl domain svc

--- services/book/service.go
+++ services/book/service.go
@@ -1,1 +1,26 @@
 package book
+
+import "golang-ddd-template/domain"
+
+type Service struct {
+	repo domain.BookRepository
+}
+
+func (s Service) AddBook(book domain.Book) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) UpdateBook(book domain.Book) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) GetBookByID(id int) (*domain.Book, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewService(repo domain.BookRepository) domain.BookService {
+	return &Service{repo}
+}
--- services/notes/service.go
+++ services/notes/service.go
@@ -1,1 +1,31 @@
 package notes
+
+import "golang-ddd-template/domain"
+
+type Service struct {
+	repo domain.NotesRepository
+}
+
+func (s Service) AddNotes(Notes domain.Notes) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) UpdateNotes(Notes domain.Notes) error {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) GetNotesByID(id int) (*domain.Notes, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func (s Service) GetNotesByBookID(bookId int) ([]domain.Notes, error) {
+	//TODO implement me
+	panic("implement me")
+}
+
+func NewService(repo domain.NotesRepository) domain.NotesService {
+	return &Service{repo}
+}
