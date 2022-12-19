df0cee466b691930d644b070669f7924e2c0893d
Yusron H <yusronzain@gmail.com>
2022-12-19 22:42:12 +0700

    add example config impl

--- config/environments.go
+++ config/environments.go
@@ -1,1 +1,13 @@
 package config
+
+import "os"
+
+type Environments struct {
+	AppPort string
+}
+
+func ENV() Environments {
+	return Environments{
+		AppPort: os.Getenv("APP_PORT"),
+	}
+}
---
+++ config/errorconst/errorconst.go
@@ -0,0 +1,7 @@
+package errorconst
+
+import "errors"
+
+var (
+	ErrBadRequest = errors.New("bad request")
+)
