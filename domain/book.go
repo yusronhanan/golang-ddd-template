f5acceca7d9af9abd2bc27d6c4058a1903d00576
Yusron H <yusronzain@gmail.com>
2022-12-19 22:42:25 +0700

    add domain contract of book and notes

--- domain/book.go
+++ domain/book.go
@@ -1,1 +1,32 @@
 package domain
+
+import (
+	"github.com/labstack/echo/v4"
+	"time"
+)
+
+type (
+	Book struct {
+		ID        int       `json:"id"`
+		Title     string    `json:"title"`
+		Owner     string    `json:"owner"`
+		CreatedAt time.Time `json:"created_at"`
+	}
+
+	BookHandler interface {
+		AddBook(c echo.Context) error
+		UpdateBook(c echo.Context) error
+		GetBookByID(c echo.Context) error
+	}
+	BookService interface {
+		AddBook(book Book) error
+		UpdateBook(book Book) error
+		GetBookByID(id int) (*Book, error)
+	}
+
+	BookRepository interface {
+		Insert(book Book) error
+		Update(book Book) error
+		GetByID(id int) (*Book, error)
+	}
+)
--- domain/notes.go
+++ domain/notes.go
@@ -1,1 +1,36 @@
 package domain
+
+import (
+	"github.com/labstack/echo/v4"
+	"time"
+)
+
+type (
+	Notes struct {
+		ID        int       `json:"id"`
+		BookID    int       `json:"book_id"`
+		Title     string    `json:"title"`
+		Body      string    `json:"body"`
+		CreatedAt time.Time `json:"created_at"`
+	}
+
+	NotesHandler interface {
+		AddNotes(c echo.Context) error
+		UpdateNotes(c echo.Context) error
+		GetNotesByID(c echo.Context) error
+		GetNotesByBookID(c echo.Context) error
+	}
+	NotesService interface {
+		AddNotes(Notes Notes) error
+		UpdateNotes(Notes Notes) error
+		GetNotesByID(id int) (*Notes, error)
+		GetNotesByBookID(bookId int) ([]Notes, error)
+	}
+
+	NotesRepository interface {
+		Insert(Notes Notes) error
+		Update(Notes Notes) error
+		GetByID(id int) (*Notes, error)
+		GetByBookID(bookId int) ([]Notes, error)
+	}
+)
